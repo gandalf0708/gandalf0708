f8b47210c532cf862d95c19b72f7ffd4d9f68df3
first <ori.finger1@gmail.com>
2024-11-20 23:36:08 +0200

    hello world

--- app/src/main/java/com/example/eggproject/MainActivity.java
+++ app/src/main/java/com/example/eggproject/MainActivity.java
@@ -1,28 +1,123 @@
 package com.example.eggproject;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.content.DialogInterface;
+
+import com.example.eggproject.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    String turn;
+    String [] [] board;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //important
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        //important
         setContentView(R.layout.activity_main);
+
+        onNewGame();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
     }
+
+    private void onNewGame () {
+        board = new String[3][3];
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                board[row][col] = new String();
+        turn = "X";
+        count = 0;
+    }
+    public void onButtonClick(View view) {
+        if(view.getId()== R.id.btn_00)
+            handleClick(0,0, R.id.btn_00);
+        if(view.getId()==R.id.btn_01)
+            handleClick(0,1,R.id.btn_01);
+        if(view.getId()==R.id.btn_02)
+            handleClick(0,2,R.id.btn_02);
+        if(view.getId()==R.id.btn_10)
+            handleClick(1,0,R.id.btn_10);
+        if(view.getId()==R.id.btn_11)
+            handleClick(1,1,R.id.btn_11);
+        if(view.getId()==R.id.btn_12)
+            handleClick(1,2,R.id.btn_12);
+        if(view.getId()==R.id.btn_20)
+            handleClick(2,0,R.id.btn_20);
+        if(view.getId()==R.id.btn_21)
+            handleClick(2,1,R.id.btn_21);
+        if(view.getId()==R.id.btn_22)
+            handleClick(2,2,R.id.btn_22);
+    }
+    private void handleClick(int row, int col, int id) {
+        if (board[row][col].equals("")) {
+            board[row][col] = turn;
+            Button btn = findViewById(id);
+            btn.setText(turn);
+            onTurnEnd();
+        }
+    }
+    private void onTurnEnd() {
+        if (isWinner())
+            endGame(turn + " won!");
+        else {
+            count++;
+            if (count == 9)
+                endGame("Tie");
+            else {
+                turn = (turn.equals("X") ? "O" : "X");
+            }
+        }
+    }
+
+    private boolean isWinner() {
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0].equals(board[row][1]) && board[row][1].equals(board[row][2]) && !board[row][0].equals(""))
+                return true;
+        }
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col].equals(board[1][col]) && board[1][col].equals(board[2][col]) && !board[0][col].equals(""))
+                return true;
+        }
+        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals(""))
+            return true;
+        if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals(""))
+            return true;
+        return false;
+    }
+
+    private void endGame(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("More Info");
+        builder.setMessage(s);
+        builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+// Exit handling
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+// Cancel handling
+
+            }
+        });
+        AlertDialog dialog = builder.show();
+    }
+
 }
